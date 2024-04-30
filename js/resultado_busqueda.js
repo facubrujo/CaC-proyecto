c410128f241feb16e5995009bf772490d4282b0a
facubrujo <facundobrujo@gmail.com>
2024-04-30 10:22:52 -0300

    pruebas de busqueda

--- js/resultado_busqueda.js
+++ js/resultado_busqueda.js
@@ -68,3 +68,20 @@ async function buscarTragos(busqueda) {
         console.error("Error al buscar tragos:", error);
     }
 }
+
+document.getElementById("formulario-busqueda").addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe de manera tradicional
+
+    const searchTerm = document.getElementById("input-busqueda").value;
+    const ruta = document.getElementById("url");
+    // const url = ruta;
+    // console.log(url);
+    if (searchTerm.trim() !== "") {
+        if (ruta !== null) {
+            window.location.href = `pages/busqueda_tragos.html?s=${searchTerm}`;
+        } else {
+            window.location.href = `busqueda_tragos.html?s=${searchTerm}`;
+        }
+    }
+});
+
