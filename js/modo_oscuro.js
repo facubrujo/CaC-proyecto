a5dd303767120afeccf8a407bf94439c7a7f6d73
ElRubenD <ruben.d.ledesma@gmail.com>
2024-05-23 19:52:11 -0300

    cambios al btn up

--- js/modo_oscuro.js
+++ js/modo_oscuro.js
@@ -20,25 +20,3 @@ const cambiarTemaClaroOscuro = (event) => {
 };
 
 toggle.addEventListener('click', cambiarTemaClaroOscuro);
-
-// Obtener el botón
-let mybutton = document.getElementById("scrollBtn");
-
-// Mostrar el botón cuando se desplaza 20px desde la parte superior
-window.onscroll = function() {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// Cuando se hace clic en el botón, se desplaza hacia arriba de la página
-function scrollToTop() {
-    document.body.scrollTop = 0; // Para Safari
-    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
-}
